4308c99385c2d58d917e6a25b5dd815c78f2fbed
Ward Vandevoort <vandevoort.ward@gmail.com>
2020-03-20 18:48:15 +0100

    Relocating of code to controllers complete

--- lab5 - Node.js/lab5/controllers/api/v1/messages.js
+++ lab5 - Node.js/lab5/controllers/api/v1/messages.js
@@ -4,4 +4,39 @@ const getAll = (req, res) => {
     });
 }
 
+const getOne = (req, res) => {
+    res.json({
+      "message": "Getting message with id " + req.params.id + " for you sir",
+    });
+}
+
+const postOne = (req, res) => {
+    res.json({
+      "message": req.body.name + " is posting a message sir",
+    });
+}
+
+const getName = (req, res) => {
+    res.json({
+      "message": "Getting your messages sir " + req.params.get(username),
+    });
+}
+
+const putOne = (req, res) => {
+    res.json({
+      "message": "Updating message with id " + req.params.id + " for you sir",
+    });
+}
+
+const deleteOne = (req, res) => {
+    res.json({
+      "message": "Deleting message " + req.params.id + " for you sir",
+    });
+}
+
 module.exports.getAll = getAll;
+module.exports.getOne = getOne;
+module.exports.postOne = postOne;
+module.exports.getName = getName;
+module.exports.putOne = putOne;
+module.exports.deleteOne = deleteOne;
--- lab5 - Node.js/lab5/routes/index.js
+++ lab5 - Node.js/lab5/routes/index.js
@@ -9,34 +9,14 @@ router.get('/', (req, res, next) => {
 
 router.get("/api/v1/messages", messagesController.getAll)
 
-router.get("/api/v1/messages/:id", function(req, res){
-  res.json({
-    "message": "Getting message with id " + req.params.id + " for you sir",
-  });
-})
+router.get("/api/v1/messages/:id", messagesController.getOne)
 
-router.get("/api/v1/messages?user=username", function(req, res){
-  res.json({
-    "message": "Getting your messages sir " + req.params.get(username),
-  });
-})
+router.get("/api/v1/messages?user=username", messagesController.getName)
 
-router.post("/api/v1/messages", function(req, res){
-  res.json({
-    "message": req.body.name + " is posting a message sir",
-  });
-})
+router.post("/api/v1/messages", messagesController.postOne)
 
-router.put("/api/v1/messages/:id", function(req, res){
-  res.json({
-    "message": "Updating message with id " + req.params.id + " for you sir",
-  });
-})
+router.put("/api/v1/messages/:id", messagesController.putOne)
 
-router.delete("/api/v1/messages/:id", function(req, res){
-  res.json({
-    "message": "Deleting message " + req.params.id + " for you sir",
-  });
-})
+router.delete("/api/v1/messages/:id", messagesController.deleteOne)
 
 module.exports = router;
