4325f18ed7127da5292fb93e49dd6fa5cab40a76
Ward Vandevoort <vandevoort.ward@gmail.com>
2020-04-02 13:40:18 +0200

    Changed to a Target ad, added new images, made basic layout, connected to a food api

--- lab4 - API/lab 4/app.js
+++ lab4 - API/lab 4/app.js
@@ -3,6 +3,7 @@ class App{
         this.getLocation();
         this.lat;
         this.lng;
+        this.warm = true;
     }
 
     getLocation(){
@@ -26,18 +27,49 @@ class App{
         }).then(data => {
             document.querySelector("#temp").innerHTML = data.currently.temperature;
             if(data.currently.temperature < 15){
-                document.querySelector("#grid-item1").src = "img/leffe-card.png";
+                /*document.querySelector("#grid-item1").src = "img/leffe-card.png";
                 document.querySelector("#beer").innerHTML = "Leffe";
                 document.querySelector(".grid-container").style.backgroundImage = "url('img/leffebg.jpg')";
-                document.querySelector("#paragraph").style.color = " #be9c56";
+                document.querySelector("#paragraph").style.color = " #be9c56";*/
+                document.querySelector("#temp").innerHTML = "Cold";
+                this.warm = false;
+                this.getMeal();
             }
             else{
-                document.querySelector("#grid-item1").src = "img/hoegaarden-card.png";
+                /*document.querySelector("#grid-item1").src = "img/hoegaarden-card.png";
                 document.querySelector("#beer").innerHTML = "Hoegaarden";
                 document.querySelector(".grid-container").style.backgroundImage = "url('img/hoegaardenbg.jpg')";
-                document.querySelector("#paragraph").style.color = "#0D155A";
-               
+                document.querySelector("#paragraph").style.color = "#0D155A";*/
+                document.querySelector("#temp").innerHTML = "Hot";
+                this.warm = true;
+                this.getMeal();
             }
+        }).catch(err => {
+            console.log(err);
+        })
+    }
+
+    getMeal(){
+
+        let recipe;
+
+        if(this.warm == true){
+          recipe = "Beefy Beer Stew";
+        }
+        else{
+          recipe = "Greek Salad Wrap";
+        }
+
+        let url2 = `https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?q=${recipe}`;
+        fetch(url2).then(response => {
+            return response.json();
+        }).then(data => {
+            console.log()
+            //document.querySelector("#temp").innerHTML = data.results[0].title;
+             /*document.querySelector("#grid-item1").src = "img/hoegaarden-card.png";
+                document.querySelector("#beer").innerHTML = "Hoegaarden";
+                document.querySelector(".grid-container").style.backgroundImage = "url('img/hoegaardenbg.jpg')";
+                document.querySelector("#paragraph").style.color = "#0D155A";*/
         }).catch(err => {
             console.log(err);
         })
